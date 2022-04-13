7689e4258443b3e8b54ecfeaabd6275775534f1a
Jane Kim <jane.kim0717@gmail.com>
2022-04-13 16:16:12 -0400

    commit tree 749d661d46f424a1cbd875d23b5e56b4d5edf54a

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-
-  resources :campers, only: [:index, :show, :create]
-  resources :activities, only: [:index, :destroy]
-  resources :signups, only: [:create]
+    resources :campers, only: [:index, :show, :create]
+    resources :activities, only: [:index, :destroy]
+    resources :signups, only: [:create]
 end
